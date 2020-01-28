d722e4d078fadd4678c1c8be3ad70b14f8a5d13f
Andrew Wilkins <afwilkin>
2020-01-28 00:32:46 -0600

    added onboard sdk

--- drone/scripts/streamer.py
+++ drone/scripts/streamer.py
@@ -9,37 +9,33 @@ context = zmq.Context()
 footage_socket = context.socket(zmq.PUSH)
 footage_socket.connect('tcp://192.168.0.8:5555')
 
+#setup camera
 camera = picamera.PiCamera()
-camera.resolution = (320, 240)
-camera.framerate = 30
-output = np.empty((240, 320, 3), dtype=np.uint8)
+camera.resolution = (640, 480)
+camera.framerate = 24
 time.sleep(2)
-#camera = cv2.VideoCapture(0)  # init the camera
+output = np.empty((480, 640, 3), dtype=np.uint8)
 
 
-with picamera.PiCamera() as camera:
-    camera.resolution = (320, 240)
-    camera.framerate = 24
-    time.sleep(2)
-    output = np.empty((240, 320, 3), dtype=np.uint8)
-    camera.capture(output, 'rgb')
+#while True:
+    #camera.capture(output, 'rgb')
+    #print("caputed")
 
 
-
-while True:
-    try:
-        print("about to caputre")
-        camera.capture(output, 'rgb')
-        print("captured")
-        #grabbed, frame = camera.read()  # grab the current frame
-        #frame = cv2.resize(frame, (640, 480))  # resize the frame
-        encoded, buffer = cv2.imencode('.jpg', output)
-        jpg_as_text = base64.b64encode(buffer)
-        footage_socket.send_string(jpg_as_text)
-        print("sent")
-        #if(grabbed):
-        #    cv2.imshow("Stream",frame)
-        #    cv2.waitKey(1)
-
-    except KeyboardInterrupt:
-        break
+#while True:
+#    try:
+#        print("about to caputre")
+#        camera.capture(output, 'rgb')
+#        print("captured")
+#        #grabbed, frame = camera.read()  # grab the current frame
+#        #frame = cv2.resize(frame, (640, 480))  # resize the frame
+#        encoded, buffer = cv2.imencode('.jpg', output)
+#        jpg_as_text = base64.b64encode(buffer)
+#        footage_socket.send_string(jpg_as_text)
+#        print("sent")
+#        #if(grabbed):
+#        #    cv2.imshow("Stream",frame)
+#        #    cv2.waitKey(1)
+#
+#    except KeyboardInterrupt:
+#        break
