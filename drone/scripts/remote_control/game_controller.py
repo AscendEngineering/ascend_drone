f5a8369e489f11c10eeb18d035e300effc5493e1
Andrew Wilkins <wilkinsaf@gmail.com>
2020-10-25 17:17:32 -0500

    new remote control script

--- drone/scripts/remote_control/game_controller.py
+++ drone/scripts/remote_control/game_controller.py
@@ -8,7 +8,7 @@ def normalize_joystick(value):
 def get_input():
 	
 	#print all events: print(event.ev_type, event.code, event.state)
-    retval = constants.control_inputs()
+    retval = {}
     
     return_input = False
     while(return_input == False):
@@ -19,25 +19,27 @@ def get_input():
             if(event.ev_type=="Absolute"):
                 return_input = True
                 if(event.code=="ABS_Y"):
-                    retval.y = -1 * normalize_joystick(event.state)
-                if(event.code=="ABS_X"):
-                    retval.x = normalize_joystick(event.state)
-                if(event.code=="ABS_Z"):
-                    retval.r = normalize_joystick(event.state)
+                    retval["y"] = -1 * normalize_joystick(event.state)
+                elif(event.code=="ABS_X"):
+                    retval["x"] = normalize_joystick(event.state)
+                elif(event.code=="ABS_Z"):
+                    retval["r"] = normalize_joystick(event.state)
 
             elif(event.ev_type == "Key"):
                 return_input = True
                 if(event.code == "BTN_BASE"):
-                    retval.z = (event.state)
+                    retval["z"] = (event.state)
                 elif(event.code == "BTN_BASE2"):
-                    retval.z = (event.state) * -1
+                    retval["z"] = (event.state) * -1
                 elif(event.code == "BTN_TOP2" and event.state==1):
-                    retval.max_rate = (event.state) * -1
+                    retval["max_rate"] = (event.state) * -1
                 elif(event.code == "BTN_PINKIE" and event.state==1):
-                    retval.max_rate =  event.state
+                    retval["max_rate"] =  event.state
                 elif(event.code == "BTN_TOP" and event.state==1):
                     print("Takeoff")
                 elif(event.code == "BTN_THUMB" and event.state==1):
                     print("Land")
+
+            #released
 
     return retval
--- drone/scripts/remote_control/remote_control.py
+++ drone/scripts/remote_control/remote_control.py
@@ -12,18 +12,19 @@ import constants
 
     
 def convert_to_proto(input_msg,rate):
+
     sendme = msgDef_pb2.msg()
     sendme.name = "any_drone"
     sendme.offset.x = input_msg.x * rate
     sendme.offset.y = input_msg.y * rate
-    sendme.offset.z = input_msg.z * rate
-    sendme.offset.yaw = input_msg.r * rate
+    sendme.offset.z = input_msg.z * rate * constants.HEIGHT_ADJUSTMENT
+    sendme.offset.yaw = input_msg.r * rate * constants.YAW_ADJUSTMENT
     sendme.offset.rate = 1
 
     return sendme
 
-def input_print(input_msg):
-    print("X:", input_msg.x, " Y:", input_msg.y, " Z:", input_msg.z, " Yaw:", input_msg.r, " Max-Rate:", input_msg.max_rate )
+def input_print(input_msg,rate):
+    print("X:", input_msg.x, " Y:", input_msg.y, " Z:", input_msg.z, " Yaw:", input_msg.r, " Max-Rate:", rate )
 
 def main():
 
@@ -49,10 +50,8 @@ def main():
         print("keboard")
 
     remote_open = True
-    send_update = True
-    last_device_input = constants.control_inputs()
-    max_rate = 0.5
-    current_rate = 0.0
+    drone_movements = constants.control_inputs()
+    max_rate = 1.0
     while(remote_open):
 
         #read input
@@ -61,21 +60,26 @@ def main():
         else:
             device_input = keyboard.get_input()
 
-        #update
-        if(not constants.same_input(last_device_input,device_input)):
+        #update controls
+        for key,val in device_input.items():
+            if(key=='x'):
+                drone_movements.x = val
+            elif(key=='y'):
+                drone_movements.y = val
+            elif(key=='z'):
+                drone_movements.z = val
+            elif(key=='r'):
+                drone_movements.r = val
+            elif(key=='max_rate'):
+                max_rate += val
+                max_rate = min(3,max_rate)
 
-            #max rate
-            if(device_input.max_rate != last_device_input.max_rate):
-                max_rate += device_input.max_rate 
-                max_rate = max(0,max_rate)
+        #adjust and send
+        proto_drone_movements = convert_to_proto(drone_movements,max_rate)
 
-            #adjust and send
-            proto_drone_movements = convert_to_proto(device_input,max_rate)
-
-            #print and replace
-            input_print(device_input)
-            last_device_input = device_input
-            socket.send(proto_drone_movements.SerializeToString())
+        #print and replace
+        input_print(drone_movements,max_rate)
+        socket.send(proto_drone_movements.SerializeToString())
             
         continue
 
